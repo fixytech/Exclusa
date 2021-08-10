604a7d8a90548e713ea7627a4263dfd237057ac0
Ezequiel Liptai <50503561+fixytech@users.noreply.github.com>
2021-08-10 12:45:25 -0300

    inicio

---
+++ ExclusaInteligente/Form1.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace ExclusaInteligente
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblMov = new System.Windows.Forms.Label();
+            this.lblPta = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblPe = new System.Windows.Forms.Label();
+            this.lblPi = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.tbCOM = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnDesconectar = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLogs = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.lblMov);
+            this.panel1.Controls.Add(this.lblPta);
+            this.panel1.Location = new System.Drawing.Point(12, 63);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(374, 132);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(365, 19);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(18, 85);
+            this.panel3.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.GreenYellow;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(333, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Puerta";
+            // 
+            // lblMov
+            // 
+            this.lblMov.AutoSize = true;
+            this.lblMov.BackColor = System.Drawing.Color.GreenYellow;
+            this.lblMov.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMov.Location = new System.Drawing.Point(2, 54);
+            this.lblMov.Name = "lblMov";
+            this.lblMov.Size = new System.Drawing.Size(19, 13);
+            this.lblMov.TabIndex = 3;
+            this.lblMov.Text = "Pir";
+            // 
+            // lblPta
+            // 
+            this.lblPta.AutoSize = true;
+            this.lblPta.BackColor = System.Drawing.Color.GreenYellow;
+            this.lblPta.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPta.Location = new System.Drawing.Point(258, 54);
+            this.lblPta.Name = "lblPta";
+            this.lblPta.Size = new System.Drawing.Size(50, 13);
+            this.lblPta.TabIndex = 2;
+            this.lblPta.Text = "Puerta ->";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Location = new System.Drawing.Point(12, 239);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(229, 284);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel5.Controls.Add(this.label4);
+            this.panel5.Controls.Add(this.lblPe);
+            this.panel5.Location = new System.Drawing.Point(20, 252);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(187, 32);
+            this.panel5.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.GreenYellow;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(333, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Puerta";
+            // 
+            // lblPe
+            // 
+            this.lblPe.AutoSize = true;
+            this.lblPe.BackColor = System.Drawing.Color.GreenYellow;
+            this.lblPe.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPe.Location = new System.Drawing.Point(4, 10);
+            this.lblPe.Name = "lblPe";
+            this.lblPe.Size = new System.Drawing.Size(86, 13);
+            this.lblPe.TabIndex = 0;
+            this.lblPe.Text = "Persiana Exterior";
+            // 
+            // lblPi
+            // 
+            this.lblPi.AutoSize = true;
+            this.lblPi.BackColor = System.Drawing.Color.GreenYellow;
+            this.lblPi.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPi.Location = new System.Drawing.Point(3, 10);
+            this.lblPi.Name = "lblPi";
+            this.lblPi.Size = new System.Drawing.Size(83, 13);
+            this.lblPi.TabIndex = 1;
+            this.lblPi.Text = "Persiana Interior";
+            // 
+            // tbCOM
+            // 
+            this.tbCOM.Location = new System.Drawing.Point(80, 20);
+            this.tbCOM.Name = "tbCOM";
+            this.tbCOM.Size = new System.Drawing.Size(140, 20);
+            this.tbCOM.TabIndex = 2;
+            this.tbCOM.Text = "COM6";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Puerto COM";
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(223, 18);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(75, 23);
+            this.btnConectar.TabIndex = 5;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btnDesconectar
+            // 
+            this.btnDesconectar.Enabled = false;
+            this.btnDesconectar.Location = new System.Drawing.Point(304, 17);
+            this.btnDesconectar.Name = "btnDesconectar";
+            this.btnDesconectar.Size = new System.Drawing.Size(82, 23);
+            this.btnDesconectar.TabIndex = 6;
+            this.btnDesconectar.Text = "Desconectar";
+            this.btnDesconectar.UseVisualStyleBackColor = true;
+            this.btnDesconectar.Click += new System.EventHandler(this.btnDesconectar_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Controls.Add(this.lblPi);
+            this.panel4.Location = new System.Drawing.Point(33, 201);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(187, 32);
+            this.panel4.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.GreenYellow;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(333, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Puerta";
+            // 
+            // txtLogs
+            // 
+            this.txtLogs.BackColor = System.Drawing.SystemColors.MenuText;
+            this.txtLogs.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.txtLogs.Location = new System.Drawing.Point(394, 82);
+            this.txtLogs.Multiline = true;
+            this.txtLogs.Name = "txtLogs";
+            this.txtLogs.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLogs.Size = new System.Drawing.Size(216, 85);
+            this.txtLogs.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(394, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Estados";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 565);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.statusStrip1.Size = new System.Drawing.Size(622, 22);
+            this.statusStrip1.TabIndex = 12;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(93, 17);
+            this.toolStripStatusLabel1.Text = "Barra de Estados";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(397, 181);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Ultimo Ingreso: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(483, 181);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 14;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(248, 255);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(362, 268);
+            this.textBox1.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(247, 239);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(30, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Logs";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 587);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtLogs);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.btnDesconectar);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbCOM);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Monitor Exclusa Inteligente";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblMov;
+        private System.Windows.Forms.Label lblPta;
+        private System.Windows.Forms.Label lblPe;
+        private System.Windows.Forms.Label lblPi;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox tbCOM;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnDesconectar;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLogs;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ ExclusaInteligente/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using System.Threading;
+
+namespace ExclusaInteligente
+{
+    public partial class Form1 : Form
+    {
+        String MP = "0";
+        String MPI = "0";
+        String MPE = "1";
+        String EP = "0";
+        String UL = "0";
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+        }
+
+       
+       
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+
+            // Asignamos las propiedades
+            serialPort1.BaudRate = 9600;
+            serialPort1.PortName = tbCOM.Text;
+           
+
+            // Controlamos que el puerto indicado esté operativo
+            try
+            {
+                // Abrimos el puerto serie
+                serialPort1.Open();
+                toolStripStatusLabel1.Text = "Dispositivo conectado!";
+                btnConectar.Enabled = false;
+                btnConectar.Text = "Conectado!";
+                btnDesconectar.Enabled = true;
+
+                Thread Hilo = new Thread(escucharSerial);
+                Hilo.Start();
+            }
+            catch
+            {
+                MessageBox.Show("Puerto incorrecto.");
+                toolStripStatusLabel1.Text = "Puerto incorrecto.";
+            }
+        }
+
+       private void escucharSerial()
+        {
+         
+            String cadena;
+            try
+            {
+          
+                while (serialPort1.IsOpen)
+                {
+                    txtLogs.Invoke(new MethodInvoker(
+                        delegate{
+                            try
+                            {
+                                
+                                cadena = serialPort1.ReadLine();
+
+                                textBox1.Text = textBox1.Text.Insert(this.textBox1.SelectionStart, DateTime.Now.ToString("hh:mm:ss") + " | " + cadena + "\r\n");
+                             
+                                try
+                                {
+                                    string[] estadoMovimiento = cadena.Split('M');
+                                    estadoMovimiento = estadoMovimiento[1].Split(',');
+                                    string EPP = estadoMovimiento[0];
+                                    if(EPP == "1")
+                                    {
+                                        EP = "1";
+                                        panel1.BackColor = Color.Red;
+                                        lblMov.Text = "PIR: Alarma!";
+                                    }
+                                    if(EPP == "0")
+                                    {
+                                        EP = "0";
+                                        panel1.BackColor = Color.LightGray;
+                                        lblMov.Text = "PIR: OK";
+                                    }
+                                }
+                                catch
+                                {
+
+                                }
+
+                                try
+                                {
+                                    string[] estadoPuerta = cadena.Split('P');
+                                    estadoPuerta = estadoPuerta[1].Split(',');
+                                    string EP = estadoPuerta[0];
+                                    
+                                    if (EP == "0")
+                                    {
+                                        MP = "1";
+                                        panel3.BackColor = Color.Red;
+                                        lblPta.Text = "Puerta: Abierta! ->";
+                                    }
+                                    if (EP == "1")
+                                    {
+                                        MP = "0";
+                                        panel3.BackColor = Color.Green;
+                                        lblPta.Text = "Puerta: Cerrada. ->";
+                                    }
+                                }
+                                catch
+                                {
+
+                                }
+
+                                try
+                                {
+                                    string[] estadoPInterior = cadena.Split('I');
+                                    estadoPInterior = estadoPInterior[1].Split(',');
+                                    string EPI = estadoPInterior[0];
+
+                                    if (EPI == "0")
+                                    {
+                                        MPI = "1";
+                                        panel4.BackColor = Color.Red;
+                                        lblPi.Text = "Persiana: Abierta!";
+                                    }
+                                    if (EPI == "1")
+                                    {
+                                        MPI = "0";
+                                        panel4.BackColor = Color.Green;
+                                        lblPi.Text = "Persiana: Cerrada.";
+                                    }
+                                }
+                                catch
+                                {
+
+                                }
+
+                                try
+                                {
+                                    string[] estadoPExterior = cadena.Split('E');
+                                    estadoPExterior = estadoPExterior[1].Split(',');
+                                    string EPE = estadoPExterior[0];
+
+                                    if (EPE == "0")
+                                    {
+                                        MPE = "1";
+                                        panel5.BackColor = Color.Red;
+                                        lblPe.Text = "Persiana: Abierta!";
+                                    }
+                                    if (EPE == "1")
+                                    {
+                                        MPE = "0";
+                                        panel5.BackColor = Color.Green;
+                                        lblPe.Text = "Persiana: Cerrada.";
+                                    }
+                                }
+                                catch
+                                {
+
+                                }
+                                try
+                                {
+                                    string[] estadoUltimoingreso = cadena.Split('U');
+                                    estadoUltimoingreso = estadoUltimoingreso[1].Split(',');
+                                    UL = estadoUltimoingreso[0];
+
+                                    if (UL == "2")
+                                    {
+                                       
+                                        
+                                        label7.Text = "PUERTA";
+                                    }
+                                    if (UL == "1")
+                                    {
+                                        label7.Text = "PERSIANA";
+                                    }
+                                }
+                                catch
+                                {
+
+                                }
+
+                                try
+                                {
+                                    string[] estadoProceso = cadena.Split('$');
+                                    estadoProceso = estadoProceso[1].Split('&');
+                                    string PRO = estadoProceso[0];
+
+                                        toolStripStatusLabel1.Text = PRO;
+                              
+                                }
+                                catch
+                                {
+
+                                }
+
+
+                                txtLogs.Text = "Movimiento: " + EP + String.Format(Environment.NewLine) + 
+                                "Puerta: " + MP + String.Format(Environment.NewLine) + 
+                                "Persiana Int: " + MPI + String.Format(Environment.NewLine) +
+                                "Persiana Ext: " + MPE + String.Format(Environment.NewLine);
+
+                            }
+                            catch(InvalidOperationException ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+                           
+                    }
+                            ));
+
+                    Thread.Sleep(1); //ms
+                }
+            }
+            catch
+            {
+
+            }
+            
+        }
+
+        private void btnDesconectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // cerramos el puerto serie
+              
+                serialPort1.Close();
+                btnConectar.Enabled = true;
+                btnDesconectar.Enabled = false;
+                btnConectar.Text = "Conectar";
+                toolStripStatusLabel1.Text = "Dispositivo desconectado.";
+
+            }
+            catch
+            {
+                MessageBox.Show("No se pudo desconectar!.");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Conecte el dispositivo";
+        }
+    }
+}
